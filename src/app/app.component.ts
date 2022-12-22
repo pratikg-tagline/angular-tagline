2d63ee2c82317322883511cf03b34637d5253620
Tagline <tagline@Taglines-iMac-Pro.local>
2022-12-22 16:21:54 +0530

    Implement data binding in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,542 @@
 import { Component } from '@angular/core';
 
+interface Lable{
+  CaseStudy:string,
+  Aboutus:string,
+  Hiredevloper:string,
+  Technology:string,
+  Industry:string,
+  mainlogo:string,
+  button:string,
+  
+}
+interface Amd{
+  madimg:string,
+  madtitle:string,
+  maddescription:string,
+  learnmore:string,
+}
+interface Agiledevops{
+  agiledevopsimg:string,
+  agiledevopstitle:string,
+  agiledevopsdescription:string,
+  learnmore:string,
+}
+interface Digitaldesign{
+  digitaldesignimg:string,
+  digitaldesigntitle:string,
+  digitaldesigndescription:string,
+  learnmore:string
+}
+interface Softwarecompany{
+  softwarwcompanyimg:string,
+  softwarwcompanytitle:string,
+  softwarwcompanydescription:string,
+  learnmore:string
+}
+
+interface Experience{
+ 
+  experiencetitle:string,
+  experiencedescription:string,
+  button:string,
+  experienceimg:string,
+}
+
+interface Professional{
+ 
+  professionalproject:string,
+  professionaltital:string,
+  
+  professionalimg:string,
+}
+interface Completedproject{
+ 
+  completedprojectproject:string,
+  completedprojecttital:string,
+  
+  completedprojectimg:string,
+}
+interface Freelancehours{
+ 
+  freelancehoursproject:string,
+  freelancehourstital:string,
+  
+  freelancehoursimg:string,
+}
+interface Yearsofexperience{
+ 
+  yearsofexperienceproject:string,
+  yearsofexperiencetital:string,
+  
+  yearsofexperienceimg:string,
+}
+interface Technologies{
+  web:string,
+  mobile:string,
+  desktop:string,
+  plateform:string,
+  database:string,
+  bigdata:string,
+  machinelearning:string,
+  devops:string,
+  clouds:string,
+
+}
+interface Backend{
+  rubiimg:string,
+  rubiheading:string,
+  pythonimg:string,
+  pythonheading:string,
+  nodeimg:string,
+  nodeheading:string,
+  phpimg:string,
+  phpheading:string,
+  javaimg:string,
+  javaheading:string,
+  golanguageimg:string,
+  golanguageheading:string
+}
+interface Frontend{
+  reactimg:string,
+  reactheading:string,
+  angularimg:string,
+  angularheading:string,
+  typescriptimg:string,
+  typescriptheading:string,
+  vuejsimg:string,
+  vuejsheading:string,
+  mateotjsimg:string,
+  mateotjsheading:string,
+  javascriptimg:string,
+  javascriptheading:string
+  htmlimg:string,
+  htmlheading:string
+}
+interface Whytegline{
+  teglinetitle:string,
+  teglinedescription:string,
+  progressiveimg:string,
+  progressivetitle:string,
+  progressiveheading:string,
+  progressivedescription:string,
+  projectmanagementimg:string,
+  projectmanagementtitle:string,
+  projectmanagementheading:string,
+  projectmanagementdescription:string,
+  engagementmodelimg:string,
+  engagementmodeltitle:string,
+  engagementmodelheading:string,
+  engagementmodeldescription:string,
+  costeffectiveimg:string,
+  costeffectivetitle:string,
+  costeffectiveheading:string,
+  costeffectivedescription:string,
+  consistentdeliveryimg:string,
+  consistentdeliverytitle:string,
+  consistentdeliveryheading:string,
+  consistentdeliverydescription:string,
+  empower:string,
+  your:string,
+  business:string,
+  withdedicateddevelopers:string,
+  button:string
+}
+interface Clientele{
+  clienteletitle:string,
+  clienteledescription1:string,
+  clienteledescription2:string,
+  clienteleheading1:string,
+  clienteledesc1:string,
+  clienteledes1:string,
+  clientelereview1:string,
+  clienteleheading2:string,
+  clienteledesc2:string,
+  clienteledes2:string,
+  clientelereview2:string,
+  clienteleheading3:string,
+  clienteledesc3:string,
+  clienteledes3:string,
+  clientelereview3:string,
+  clienteleheading4:string,
+  clienteledesc4:string,
+  clienteledes4:string,
+  clientelereview4:string,
+  clienteleheading5:string,
+  clienteledesc5:string,
+  clienteledes5:string,
+  clientelereview5:string,
+  clienteleheading6:string,
+  clienteledesc6:string,
+  clienteledes6:string,
+  clientelereview6:string,
+}
+interface Awards{
+  awardstitle1:string,
+  awardstitle2:string,
+  awardsimg1:string,
+  awardsimg2:string,
+  awardsimg3:string,
+  awardsimg4:string,
+  awardsimg5:string,
+}
+
+interface Ourblogs{
+  blogshadding:string,
+  pythonimg:string,
+  pythonbtn:string,
+  pythondate:string,
+  pythontitle:string,
+  pythondescription:string,
+  pythonread:string
+  nodejsimg:string,
+  nodejsbtn:string,
+  nodejsdate:string,
+  nodejstitle:string,
+  nodejsdescription:string,
+  nodejsread:string
+  rorimg:string,
+  rorbtn:string,
+  rordate:string,
+  rortitle:string,
+  rordescription:string,
+  rorread:string
+
+}
+interface Globalpresence{
+  firstheading:string,
+  firstdescription:string,
+  firstnum:string,
+  secondheading:string,
+  seconddescription:string,
+  secondnum:string,
+  thirdheading:string,
+  thirddescription:string,
+  thirdnum:string, 
+  read:string 
+}
+interface Forms{
+  formsheading:string,
+  formbtn:string
+}
+
+interface Footer{
+  footerheading:string;
+  footerdesc1:string;
+  footerdesc2:string;
+  footerdesc3:string;
+  footerdesc4:string;
+  footerbtn:string;
+  footerdes:string;
+  footerhrhadding:string;
+  footeremail:string;
+  footerhrnum:string;
+  footersaleshadding:string;
+  footersalesemail:string;
+  footersalesname:string;
+  footerhelp:string;
+  footercareer:string;
+  footeraboutus:string;
+  footercontact:string;
+  footerblog:string;
+  footerprivacy:string;
+  footersitemap:string;
+  footerservices:string;
+  footermobile:string;
+  footerdevelopment:string;
+  footerdesign:string;
+  footerui:string;
+  footerdigitalmarketing:string;
+  footerhiredevelopers:string;
+  footerpython:string;
+  footerangular :string;
+  footerror:string;
+  footerflutter:string;
+  footerreact:string;
+  footerreactnative:string;
+  footerfollow:string;
+  footer2022:string;
+  footerreserved:string;
+  footercompanyname:string;
+  
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
 })
 export class AppComponent {
   title = 'Angular-Tagline';
+ 
+  lable :Lable ={
+    CaseStudy:'casestady',
+    Aboutus:'aboutus',
+    Hiredevloper: 'hiredevloper',
+    Technology:'technology',
+    Industry:'industry',
+    mainlogo:"../assets/../assets/images/tagline.png",
+    button:"button"
+  }
+  maintitle:string='Software Development Company & IT Consultancy'
+  starting:string='Serving Start-UPs To Fortune 500 '
+  firstp:string='With the help of Innovation and Technology, We give you the power to <br> change the world with yourideas.'
+  clientimg:string='../assets/images/images.png'
+  OurServices:string='OurServices'
+  rightforyou:string='rightforyou'
+  Findthe:string='Findthe'
+
+  Amd:Amd={
+  madimg:"../assets/../assets/images/web-design-icon.webp",
+  madtitle:'Software Product Engineering',
+  maddescription:'We are dedicated to providing the best Software product engineering services for your business. Ourteam of developers provides the service of software product engineering and it includes designing,developing, testing, and deploying the software products',
+  learnmore:'Learn More ->',
+  }
+  agiledevops:Agiledevops={
+    agiledevopsimg:"../assets/../assets/images/web-design-icon.webp",
+    agiledevopstitle:'Agile QA, Automation, and DevOps',
+    agiledevopsdescription:'At Tagline Infotech, we have a team of dedicated developers that provide the services like Agile QA,Automation, and DevOps. Our DevOps engineers constantly do the testing of software while Agile QAensures the quality of the software',
+    learnmore:'Learn More ->',
+  }
+  digitaldesign:Digitaldesign={
+    digitaldesignimg:"../assets/../assets/images/uiux-design-icon.webp",
+    digitaldesigntitle:'Digital Strategy & Design',
+    digitaldesigndescription:'Tagline Infotech helps clients to create a clear digital strategy and design for the growth of theirbusinesses. Our developers and UI UX designers put the user first to create easy-to-navigate interfaces and smooth end-to-end experiences',
+    learnmore:'Learn More ->'
+  }
+  softwarecompany:Softwarecompany={
+    softwarwcompanyimg:"../assets/../assets/images/software-development-1.png",
+    softwarwcompanytitle:'Software outsourcing company',
+    softwarwcompanydescription:'Tagline Infotech is an experienced software outsourcing company that provides services for variousbusinesses. We have a reliable team of developers that provides high-quality outsourcing services to scale your business.',
+    learnmore:'Learn More ->'
+  }
+  dropline1:string='Don not know what to choose?'
+  dropline2:string='Drop us a line.'
+  contactus:string='ContactUs'
+
+  experience:Experience={
+ 
+    experiencetitle:'We have the experience',
+    experiencedescription:'We have got the experience to make your dream come true, and these stats speak for themselves: we have150+ clients worldwide in only three years, and we have worked on over 200 projects, all of which havebeen finished successfully and on schedule, with 100% client satisfaction. We also have 60K+ hours of freelancing experience',
+    button:'button',
+    experienceimg:"../assets/../assets/images/woman.webp",
+  }
+  professional:Professional={
+ 
+    professionalproject:'125+',
+    professionaltital:'Professional',
+    
+    professionalimg:"../assets/../assets/images/contentlogo.webp",
+  }
+  completedproject:Completedproject={
+ 
+    completedprojectproject:'450+',
+    completedprojecttital:"Completedproject",
+    
+    completedprojectimg:"../assets/../assets/images/contentlogo2.webp",
+  }
+  freelancehours:Freelancehours={
+ 
+    freelancehoursproject:'150+',
+    freelancehourstital:'Freelancehours',
+    
+    freelancehoursimg:"../assets/../assets/images/contentlogo3.webp",
+  }
+  yearsofexperience:Yearsofexperience={
+   
+    yearsofexperienceproject:'+5',
+    yearsofexperiencetital:'Yearsofexperience',
+    
+    yearsofexperienceimg:"../assets/../assets/images/contentlogo4.webp",
+  }
+  ourtechnologies:string='Our Technologies'
+  technologies:Technologies={
+    web:'Web',
+    mobile:'Mobile',
+    desktop:'Desktop',
+    plateform:'Plateform',
+    database:'DetaBase',
+    bigdata:'BigData',
+    machinelearning:'MachineLearning',
+    devops:'DevOps',
+    clouds:'Clouds',
+  }
+  web:string='Web'
+  backend:Backend={
+    rubiimg:'../assets/../assets/images/ror.png',
+    rubiheading:'Ruby on Rails',
+    pythonimg:"../assets/../assets/images/python.png",
+  pythonheading:'Python',
+  nodeimg:"../assets/../assets/images/node_js.svg",
+  nodeheading:'Node Js',
+  phpimg:"../assets/../assets/images/php.png",
+  phpheading:'PHP',
+  javaimg:"../assets/../assets/images/android-java.png",
+  javaheading:'Java',
+  golanguageimg:"../assets/../assets/images/go.png",
+  golanguageheading:'Go Language'
+  }
+  backendname:string='Back End'
+  frontend: Frontend={
+    reactimg:'../assets/../assets/images/react-js.png',
+    reactheading:'React Js',
+    angularimg:'../assets/../assets/images/angular-icon.png',
+    angularheading:'Angular',
+    typescriptimg:'../assets/../assets/images/typescript-icon.png',
+    typescriptheading:'Typescript',
+    vuejsimg:'../assets/../assets/images/vue-js.png',
+    vuejsheading:'Vue Js',
+    mateotjsimg:'../assets/../assets/images/meter-js.png',
+    mateotjsheading:'Mateor Js',
+    javascriptimg:'../assets/../assets/images/javascript-icon.png',
+    javascriptheading:'Java Script',
+    htmlimg:'../assets/../assets/images/html5.png',
+    htmlheading:'HTML 5'
+  }
+  whytegline: Whytegline={
+    teglinetitle:'Why Choose Tagline Infotech ?',
+    teglinedescription:'We have a team of developers who use their expertise to provide complete web development,mobile app development,testing & assurance, SaaS development, and designing services. Tagline Infotech has a team of developers thatyou can hiby mentioning the requirements for your project. Save time and money by hiring our developers foryour startup or large-scale company. Our team of developers has the experience to understand the business ideasof the clients to provide solutions that focus on the core values of the business. Here you will find somebenefits of hiring developers from us.',
+    progressiveimg:'../assets/../assets/images/choose-roadmap.png',
+  progressivetitle:'Progressive Roadmap',
+  progressiveheading:'01',
+  progressivedescription:'Our team of developers creates a progressive roadmap that includes a strategic plan to define a goal andmajor steps to reach that goal. We have a team of developers that uses progressive',
+  projectmanagementimg:'../assets/../assets/images/choose-project.png',
+  projectmanagementtitle:'Efficient Project Management',
+  projectmanagementheading:'02',
+  projectmanagementdescription:'We believe in efficient project management and our experienced developers use project management tools like Wrike and Zoho. Our developers use these project management tools efficiently',
+  engagementmodelimg:'../assets/../assets/images/choose-flexible.svg',
+  engagementmodeltitle:'Flexible Engagement Model',
+  engagementmodelheading:'03',
+  engagementmodeldescription:'We provide flexible engagement models so that clients can hire developers for their projects. Theseengagement models are flexible so if you aren’t satisfied with any model you can switch to another.',
+  costeffectiveimg:'../assets/images/choose-cost.png',
+  costeffectivetitle:'Cost-Effective',
+  costeffectiveheading:'04',
+  costeffectivedescription:'Our team of developers provides the best development services at affordable prices. We believe thathigh-quality solutions can be delivered at cost-effective prices and that’s why we provide different hiring',
+  consistentdeliveryimg:'../assets/images/choose-delivery.png',
+  consistentdeliverytitle:'Consistent Delivery',
+  consistentdeliveryheading:'05',
+  consistentdeliverydescription:'Our developers follow an agile approach to deliver the project before the deadline. We are providingconsistent delivery of fully-featured projects as per the client’s requirements.',
+  empower:'Empower',
+  your:'Your ',
+  business:'Business',
+  withdedicateddevelopers:'with Dedicated Developers',
+  button:'Talk to our Expert',
+  }
+  clientele: Clientele={
+    clienteletitle:'Words From Our Clientele',
+    clienteledescription1:'Good things come for those who work hard, and thats why our clientele appreciate our efforts. Tagline Infotech provides the best services to its clients, and it is reflected through our ratings and reviewson various platforms.',
+    clienteledescription2:'We are a well-known IT services company on Clutch. Check out what our customers have to say about ourservices. Read their ratings, feedback, and stories about partnering with us before making your own decision.',
+    clienteleheading1:'5.0',
+  clienteledesc1:'Everything always turns out perfectly done.',
+  clienteledes1:'IT & Computer Scientist, Self-Employed',
+  clientelereview1:'Verified Review',
+  clienteleheading2:'5.0',
+  clienteledesc2:'They understand complex technical requirements, and they have a team that quickly gets up to speed.',
+  clienteledes2:'Founder, Cimpro.io',
+  clientelereview2:'Verified Review',
+  clienteleheading3:'5.0',
+  clienteledesc3:'…theyre able to tackle pretty much everything you throw at them.',
+  clienteledes3:'CEO, Promoprep',
+  clientelereview3:'Verified Review',
+  clienteleheading4:'5.0',
+  clienteledesc4:'When things go wrong, they were good at adapting and problem solving on the fly.',
+  clienteledes4:'CEO, SupplementSnoop',
+  clientelereview4:'Verified Review',
+  clienteleheading5:'5.0',
+  clienteledesc5:'The team takes up new challenges and finishes the project on time.',
+  clienteledes5:'Software Architect, Specialty Finance Company',
+  clientelereview5:'Verified Review',
+  clienteleheading6:'5.0',
+  clienteledesc6:'We are getting close to launch now and its been a tremendous help to have their developers on my team.',
+  clienteledes6:'Owner, e-Commerce Support Company',
+  clientelereview6:'Verified Review',
+  }
+  awards: Awards={
+    awardstitle1:'Awards &',
+    awardstitle2:' Recognitions',
+    awardsimg1:'../assets/images/award-2.webp',
+    awardsimg2:'../assets/images/award-3.webp',
+    awardsimg3:'../assets/images/award-4.webp',
+    awardsimg4:'../assets/images/award-5.webp',
+    awardsimg5:'../assets/images/award-6.webp',
+
+  }
+  mainblogsname:string='Our Global Presence'
+  ourblogs :Ourblogs={
+    blogshadding:'Our Blogs',
+    pythonimg:'../assets/images/Python-vs-Java.png',
+    pythonbtn:'Python',
+    pythondate:'15/12/22',
+    pythontitle:'Python vs Java Comparison Of',
+    pythondescription:'Quick Summary The most difficult decision for an entrepreneur is deciding on a programming...',
+    pythonread:'Read More ->',
+    nodejsimg:'../assets/images/Node.js-Application.png',
+    nodejsbtn:'Node Js',
+    nodejsdate:'9/12/22',
+    nodejstitle:'Best Practices for Node.js Application Development:...',
+    nodejsdescription:'Quick Summary Want to know about node js architecture and node js architecture best',
+    nodejsread:'Read More ->',
+    rorimg:'../assets/images/Kotlin-vs-Java.png',
+    rorbtn:'RoR',
+    rordate:'9/12/22',
+    rortitle:'JavaScript vs Ruby On Rails: What...',
+    rordescription:'Quick Summary: Ruby on Rails and JavaScript both languages are both widely popular among',
+    rorread:'Read More ->',
+  
+  }
+  globalpresence:Globalpresence={
+    firstheading:'India (HQ)',
+    firstdescription:'Digital Valley, 423, Apple Square, beside Lajamni Chowk, Mota Varachha, Surat,Gujarat 394101',
+    firstnum:' +91 9913 808 285',
+    secondheading:'U.S.A',
+    seconddescription:'2885 Old Zion, Cemetery Rd, Loganville, Ga 30052',
+    secondnum:'+1 (404) 483-3388',
+    thirdheading:'United Kingdom',
+    thirddescription:'65 Byron road North wembley Middlesex, london HA0 3PB',
+    thirdnum:'+44 78240 75400',  
+    read:'solutions@taglineinfotech.com',
+  }
+  forms: Forms={
+    formsheading:'Get Your Free Quote Today',
+    formbtn:'Send Message'
+  }
+  footer:Footer={
+  footerheading:'Get latest from Tagline Infotech',
+  footerdesc1:'Share your email so we can send you',
+  footerdesc2:'guides',
+  footerdesc3:'and',
+  footerdesc4:'industry news.',
+  footerbtn:'Subscribe',
+  footerdes:'You can unsubscribe at any time. Read our privacy policy.',
+  footerhrhadding:'HR Inquiry',
+  footeremail:'hr@taglineinfotech.com',
+  footerhrnum:'+91 6354362521',
+  footersaleshadding:'Sales Inquiry',
+  footersalesemail:'solutions@taglineinfotech.com',
+  footersalesname:' Tagline infotech',
+  footerhelp:'Help & Advice',
+  footercareer:'Career',
+  footeraboutus:'About Us',
+  footercontact:'Contact Us',
+  footerblog:'Blog',
+  footerprivacy:'Privacy pilicy',
+  footersitemap:'Sitemap',
+  footerservices:'Services',
+  footermobile:'Mobile',
+  footerdevelopment:'web Development',
+  footerdesign:'Web Design',
+  footerui:'UI / UX Design',
+  footerdigitalmarketing:'Digital Marketing',
+  footerhiredevelopers:'Hire Developers',
+  footerpython:'Hire Python Developer',
+  footerangular :'Hire Angular Developer',
+  footerror:'Hire ROR Developer',
+  footerflutter:'Hire Flutter Developer',
+  footerreact:'Hire React Js Developer',
+  footerreactnative:'Hire React Native Developer',
+  footerfollow:'Follow Us',
+  footer2022:'2022',
+  footerreserved:'All rights reserved by',
+  footercompanyname:'Tagline Infotech LLP',
+  }
 }
